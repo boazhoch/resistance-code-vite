906a301de475344b01cf32d75b25c0592271b0db
Boaz hoch <boazmier@gmail.com>
2023-03-17 11:47:22 +0200

    modify vite config

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+// init.js
+window.global ||= window;
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import resolve from "@rollup/plugin-node-resolve";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  define: {
+    global: "window",
+  },
+  plugins: [
+    react(),
+    {
+      ...resolve({
+        preferBuiltins: false,
+        browser: true,
+      }),
+      enforce: "pre",
+      apply: "build",
+    },
+  ],
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
+});

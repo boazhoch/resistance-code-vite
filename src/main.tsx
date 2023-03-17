1d6cacc0253049bea5f8084ea40eb8eaf549cb0d
Boaz hoch <boazmier@gmail.com>
2023-03-17 11:07:41 +0200

    done

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Amplify } from "aws-amplify";
+import { AmplifyProvider } from "@aws-amplify/ui-react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//@ts-ignore
+import config from "./aws-exports";
+
+Amplify.configure(config);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AmplifyProvider>
+      <App />
+    </AmplifyProvider>
+  </React.StrictMode>
+);
